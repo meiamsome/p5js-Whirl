cdceb5dbd8e17ef903d1b8784f46fb3947ed6d72
Oliver Wright <meiamsome@meiamso.me>
2017-02-11 23:23:06 +0000

    Nice, but not right

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+var angle = 10;
+var scaleFactor;
+
 function setup() {
-  
+  createCanvas(400, 400);
+  setFrameRate(8);
+
+  scaleFactor =  cos(radians(angle));
 }
 
 function draw() {
-  
+  translate(width / 2, height /2);
+  rotate(frameCount * angle);
+  scale(pow(scaleFactor, frameCount));
+  rect(- width / 2, - height / 2, width, height);
 }
