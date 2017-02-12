7b4f81ece2dcf98d2d07cce35c65aa0f56aca741
Oliver Wright <meiamsome@meiamso.me>
2017-02-12 00:36:05 +0000

    Super swaggy

--- sketch.js
+++ sketch.js
@@ -1,16 +1,43 @@
-var angle = 10;
-var scaleFactor;
+var cSize, degree, angle, scaleFactor, lastBreak;
 
 function setup() {
-  createCanvas(400, 400);
-  setFrameRate(8);
+  cSize = min(screen.width, screen.height);
+  angle = radians(2);
+  setDegree(3);
+  lastBreak = 0;
 
-  scaleFactor =  cos(radians(angle));
+  createCanvas(cSize, cSize);
+  setFrameRate(30);
+  colorMode(HSB);
+  background(0);
+  noFill();
+}
+
+function setDegree(deg) {
+  degree = deg;
+  var theta = (degree - 2) * PI / degree
+  scaleFactor = sin(theta) / (sin(angle) + sin(PI - theta - angle));
+}
+
+function drawPoly() {
+  beginShape();
+  for(var i = 0; i < degree; i++) {
+    vertex(cSize * cos(i * TWO_PI / degree) / 2, cSize * sin(i * TWO_PI / degree) / 2);
+  }
+  endShape(CLOSE);
 }
 
 function draw() {
+  var f = frameCount - lastBreak;
+  if(f % (degree * 30) == 0) {
+    background(0);
+    setDegree(degree + 1);
+    lastBreak = frameCount;
+    return;
+  }
+  stroke(frameCount % 360, 100, 100);
   translate(width / 2, height /2);
-  rotate(frameCount * angle);
-  scale(pow(scaleFactor, frameCount));
-  rect(- width / 2, - height / 2, width, height);
+  rotate(f * angle);
+  scale(pow(scaleFactor, f));
+  drawPoly();
 }
